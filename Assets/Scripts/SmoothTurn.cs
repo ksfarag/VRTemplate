dc90d8c36f43c81eb1fa8cb9b1046b18c77af484
Kerolos Farag <72260733+ksfarag@users.noreply.github.com>
2023-02-22 23:43:08 -0330

    Replace unity turn script with a custom one for smoother turning

---
+++ Assets/Scripts/SmoothTurn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SmoothTurn : MonoBehaviour
+{
+    //Simple script that reads a vector2 input action and rotate the parent object on the y-axis based on the action value
+    public InputActionProperty RotateAction;
+    public int TurnSpeed = 1;
+    void Update()
+    {
+        float turnValue = RotateAction.action.ReadValue<Vector2>().x;
+        transform.Rotate(0, turnValue * TurnSpeed, 0);
+    }
+}
