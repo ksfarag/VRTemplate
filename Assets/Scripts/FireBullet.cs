1ce0dbe7bf4ee0dd956d79e8e35f3c31c2d65d47
Kerolos Farag <72260733+ksfarag@users.noreply.github.com>
2023-02-27 02:00:40 -0330

    Implemented FullyAuto shooting behavior + general polishes

--- Assets/Scripts/FireBullet.cs
+++ Assets/Scripts/FireBullet.cs
@@ -1,30 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class FireBullet : MonoBehaviour
 {
-    public GameObject bullet;
-    public GameObject barrel;
-    public float velocity;
-    void Start()
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject barrel;
+    [SerializeField] private float velocity;
+    [SerializeField] private bool FullAuto;
+    [SerializeField] private int MagSize;
+    [SerializeField] private float FireRate;
+
+    private bool TriggerPressed = false;
+    private bool CanShootAgain = false;  //used to manage rate of fire
+    private float TimePassedSinceLastBullet;
+
+    private void Start()
     {
-        XRGrabInteractable grabable = GetComponent<XRGrabInteractable>();
-        grabable.activated.AddListener(Shoot);
+        TimePassedSinceLastBullet = 0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (TriggerPressed)
+        {
+            Shoot();
+        }
+        TimePassedSinceLastBullet += Time.deltaTime;
+
+        if (TimePassedSinceLastBullet >= 1/FireRate)
+        {
+            CanShootAgain = true;
+        }
+        else
+        {
+            CanShootAgain = false;
+        }
     }
 
-    public void Shoot(ActivateEventArgs arg) 
+    public void TriggerPress() //This method is called by the XR Grab Interactable script (Specifically by the Activated event)
     {
+        TriggerPressed = true;
+    }
+
+    public void Triggerrelease() //This method is called by the XR Grab Interactable script
+    {
+        TriggerPressed = false;
+    }
+
+    private void Shoot() 
+    {
+        if (!CanShootAgain) { return; }
+        TimePassedSinceLastBullet = 0f;
         GameObject newBullet = Instantiate(bullet);
         newBullet.transform.position = barrel.transform.position;
         newBullet.GetComponent<Rigidbody>().velocity = barrel.transform.forward* velocity;
         Destroy(newBullet, 5);
+        if (!FullAuto ) { TriggerPressed = false; } // If gun is not FullAuto, Release trigger after shooting once
     }
+
+
 }
